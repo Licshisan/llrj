ea6917bde8635c000240c2b25bb246df92f8600c
Chris Lee <licshisan@gmail.com>
2026-03-29 17:54:37 +0800

    藏品

--- assets/脚本/默认内容/加载内容.ts
+++ assets/脚本/默认内容/加载内容.ts
@@ -6,12 +6,13 @@ import { 默认套餐表 } from "./套餐表";
 import { 注册钩子 } from "../管理器/钩子管理器";
 import { 默认效果表 } from "./效果表";
 import { 默认制作表 } from "./制作表";
-import { 伙伴特性定义类型, 默认伙伴特性表 } from "./伙伴特性表";
+import { 默认伙伴特性表 } from "./伙伴特性表";
 import { 默认特性表 } from "./特性表";
 import { 默认天赋表 } from "./天赋表";
 import { error, log } from "cc";
 import { 默认难度表 } from "./难度表";
 import { 默认特质表 } from "./特质表";
+import { 默认藏品表 } from "./藏品表";
 
 interface WindowWithErrorHandler extends Window {
 	__errorHandler?: (name: string, line: number, msg: string, stack: string) => void;
@@ -83,6 +84,14 @@ export function 注册钩子函数() {
 		}
 	});
 
+	默认藏品表.forEach((藏品项) => {
+		const 效果 = 藏品项.效果
+		if (效果) {
+			for (let 时机 in 效果) {
+				注册钩子(时机, 效果[时机])
+			}
+		}
+	});
 }
 export async function 加载游戏内容() {
 	if (加载完成) return
