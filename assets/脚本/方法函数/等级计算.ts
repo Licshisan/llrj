c4a9265d472db97f4a75afac5eacbb0f6e3a21e6
Chris Lee <licshisan@gmail.com>
2026-05-10 19:20:07 +0800

    todos

--- assets/脚本/方法函数/等级计算.ts
+++ assets/脚本/方法函数/等级计算.ts
@@ -2,16 +2,18 @@ import { 执行钩子 } from "../管理器/钩子管理器";
 
 export interface 等级计算容器类型 {
     是否解锁: boolean,
+    基础等级: number,
     额外等级: number,
 }
 
 export function 计算等级(计算类型: string, 计算名称: string): number {
     const 等级计算容器: 等级计算容器类型 = {
         是否解锁: false,
+        基础等级: 1,
         额外等级: 0,
     };
     执行钩子(`计算${计算类型}等级`, [计算名称, 等级计算容器]);
-    return 等级计算容器.是否解锁 ? 1 + 等级计算容器.额外等级 : 0
+    return 等级计算容器.是否解锁 ? 等级计算容器.基础等级 + 等级计算容器.额外等级 : 0
 }
 
 export const 计算天赋等级 = (计算名称: string) => 计算等级("天赋", 计算名称)
