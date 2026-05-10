8fea4bebccd9c429e6bc71a3c4cec956cd7bc2f6
Chris Lee <licshisan@gmail.com>
2026-05-10 16:53:33 +0800

    等级计算

--- assets/脚本/管理器/玩家管理器.ts
+++ assets/脚本/管理器/玩家管理器.ts
@@ -8,11 +8,8 @@ export const 默认玩家 = {
 	ext_info: {
 		战胜语: "",
 		战败语: "",
-		成就: {} as Record<string, number>,
-		特质: {} as Record<string, number>,
+		成就: {} as Record<string, any>,
 		藏品: {} as Record<string, number>,
-		技能: {} as Record<string, number>,
-		补偿: {} as Record<string, number>,
 	} as any,
 	server_info: {} as 服务器信息,
 	created_at: "",
